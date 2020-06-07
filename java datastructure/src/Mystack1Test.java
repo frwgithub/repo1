12d35df9f30874c3ebd5cfcbf0616ffcf4d141dd
furenwei <3221723757@qq.com>
2020-06-07 11:57:44 +0800

    ppp

---
+++ java datastructure/src/Mystack1Test.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class Mystack1Test {
+    public static void main(String[] args) {
+        @Test
+        public void testPush(){
+            Mystack1 mystack1 = new Mystack1();
+            mystack1.push(100);
+        }
+    }
+}
